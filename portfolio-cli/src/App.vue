785870ef457b70212863ac72a4485abed03a96b6
Carlos Aguilera <18aguilerac@gmail.com>
2022-08-21 15:00:48 -0700

    init

---
+++ portfolio-cli/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav class="nav justify-content-center">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
+</template>
