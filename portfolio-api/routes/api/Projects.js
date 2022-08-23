b23c8a260e6345cde1ea528ca62c75c4e2f48f49
Carlos Aguilera <18aguilerac@gmail.com>
2022-08-22 21:11:35 -0700

    added project by name api call

--- portfolio-api/routes/api/Projects.js
+++ portfolio-api/routes/api/Projects.js
@@ -13,4 +13,14 @@ router.get('/:category', async (req, res) => {
   }
 });
 
+router.get('/name/:name', async (req, res) => {
+  const name = req.params.name;
+  try {
+    const Query = await Project.find({ name });
+    res.status(200).json(Query);
+  } catch (err) {
+    res.status(404).json({ err });
+  }
+});
+
 module.exports = router;
