8f5f67924dfc38b83a6b439eb5b8c092e2c38bac
Carlos Aguilera <18aguilerac@gmail.com>
2022-08-22 23:06:13 -0700

    yes sir

--- portfolio-cli/src/views/Project.vue
+++ portfolio-cli/src/views/Project.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="container text-white mt-5 pt-5">
-    <div class="row border d-flex justify-content-between">
-      <button class="btn btn-sm col-1 text-muted active" @click="$router.push('/')">
+    <div class="row d-flex justify-content-between">
+      <button
+        class="btn btn-sm col-2 ms-3 col-lg-1 text-muted active"
+        @click="$router.push('/')"
+      >
         &#8592;
       </button>
-      <div class="dropdown-center col-2">
+      <div class="dropdown-center col-3 col-lg-2">
         <button
           class="btn btn-sm text-muted active dropdown-toggle"
           type="buttn"
@@ -14,13 +17,13 @@
         </button>
         <ul class="dropdown-menu bg-dark text-center">
           <li>
-            <a :href="ProjectCodeLink" class="dropdown-item text-white" target="_blank">
+            <a :href="project.code_link" class="dropdown-item text-white" target="_blank">
               Code Repository
             </a>
           </li>
           <li v-if="ProjectHasLiveTest">
             <a
-              :href="ProjectTestLink"
+              :href="project.test_link"
               class="dropdown-item text-white"
               type="button"
               target="_blank"
@@ -29,6 +32,33 @@
           </li>
         </ul>
       </div>
+    </div>
+    <div class="row mt-4 pb-5">
+      <h2 class="text-center col-12" v-text="project.name"></h2>
+      <video :src="project.video_path" class="mt-4" autoplay muted loop></video>
+    </div>
+    <div class="row mt-5">
+      <h5 class="text-center text-muted">About The Project</h5>
+      <p class="text-center" v-text="project.description"></p>
+    </div>
+    <div class="row mt-5">
+      <h5 class="text-center text-muted">Creation</h5>
+      <p class="text-center" v-text="project.creation"></p>
+    </div>
+    <p v-if="project.name === 'Pokemon-Clone'" class="text-center">
+      <small class="text-muted"
+        >With
+        <a
+          href="https://github.com/connor-darling"
+          class="btn btn-sm text-muted p-0"
+          target="_blank"
+        >
+          Connor Darling
+        </a>
+      </small>
+    </p>
+    <div class="row mt-5 mb-5">
+      <p class="text-center" v-text="project.date"></p>
     </div>
   </div>
 </template>
@@ -40,21 +70,25 @@ export default {
   name: 'Project-V',
   data() {
     return {
-      project: [],
+      project: {},
       ProjectHasLiveTest: false,
-      ProjectCodeLink: '',
-      ProjectTestLink: '',
     };
   },
   async mounted() {
-    this.project = await Project.GetProjectByName(this.$route.params.project);
+    const res = await Project.GetProjectByName(this.$route.params.project);
+    this.project = {
+      name: res[0].name,
+      video_path: res[0].video_path,
+      description: res[0].description,
+      creation: res[0].mini_description,
+      code_link: res[0].code_link,
+      test_link: res[0].test_link,
+      date: res[0].date,
+    };
 
-    this.ProjectTestLink = this.project[0].test_link;
-    if (this.ProjectTestLink) {
+    if (this.project.test_link) {
       this.ProjectHasLiveTest = true;
     }
-
-    this.ProjectCodeLink = this.project[0].code_link;
   },
 };
 </script>
