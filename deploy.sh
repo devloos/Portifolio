105a04c08a0506dd725a1aabbb04661275c2479d
Carlos Aguilera <18aguilerac@gmail.com>
2023-06-17 10:21:41 -0700

    clean slate

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-npm run build
-
-rm -rf /Users/ca/Developer/Projects/Portfolio/portfolio-api/dist
-mv dist /Users/ca/Developer/Projects/Portfolio/portfolio-api
--- src/App.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <!-- TODO: nav -->
-  <div class="container col-md-7 col-lg-7 col-xl-5">
-    <router-view />
-  </div>
-</template>
