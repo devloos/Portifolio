6c2312c57b32421c5cc63e0f6e83d2d30f9431bd
Carlos Aguilera <18aguilerac@gmail.com>
2023-06-20 20:31:28 -0700

    prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,18 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
 module.exports = {
   root: true,
   env: {
     node: true,
     'vue/setup-compiler-macros': true,
   },
-  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-prettier/skip-formatting',
+  ],
   rules: {
     'vue/multi-word-component-names': 'off',
-  },
-  parserOptions: {
-    ecmaVersion: 'latest',
   },
 };
