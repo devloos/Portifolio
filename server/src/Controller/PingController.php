060481218a90435588c068dd9193a209dd688301
Carlos Aguilera <18aguilerac@gmail.com>
2024-07-09 01:17:11 -0700

    update

--- server/src/Controller/PingController.php
+++ server/src/Controller/PingController.php
@@ -4,15 +4,16 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PingController extends AbstractController
 {
     #[Route('/api/ping', name: 'Ping')]
-    public function ping(): JsonResponse
+    public function ping(Request $request): JsonResponse
     {
         return $this->json([
-            'message' => 'Pong',
+            'message' => 'Pong ' . $request->query->has('lol') ? 'TRUE' : 'FALSE',
             'success' => true,
         ]);
     }
