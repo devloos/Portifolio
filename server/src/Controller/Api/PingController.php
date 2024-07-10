62691a27840b9723e9f670f1fbba442431d6e856
Carlos Aguilera <18aguilerac@gmail.com>
2024-07-10 00:04:39 -0700

    move ping

--- server/src/Controller/Api/PingController.php
+++ server/src/Controller/Api/PingController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class PingController extends AbstractController
 {
-    #[Route('/ping', name: 'Ping')]
+    #[Route('/api/ping', name: 'Ping')]
     public function ping(Request $request): JsonResponse
     {
         return $this->json([
@@ -18,7 +18,7 @@ class PingController extends AbstractController
         ]);
     }
 
-    #[Route('/ping/database', name: 'Ping Database')]
+    #[Route('/api/ping/database', name: 'Ping Database')]
     public function pingDatabase(): JsonResponse
     {
         return $this->json([
