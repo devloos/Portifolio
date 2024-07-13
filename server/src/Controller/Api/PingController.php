8dc11546463935a3a85511c92f91e51ab3d4da0b
Carlos Aguilera <18aguilerac@gmail.com>
2024-07-12 22:36:01 -0700

    update

--- server/src/Controller/Api/PingController.php
+++ server/src/Controller/Api/PingController.php
@@ -4,11 +4,9 @@ namespace App\Controller\Api;
 
 use App\Entity\Ping;
 use App\Repository\PingRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PingController extends AbstractController
--- server/src/Controller/DefaultController.php
+++ server/src/Controller/DefaultController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
 
-class ErrorController extends AbstractController
+class DefaultController extends AbstractController
 {
+    #[Route('/{path}', requirements: ['path' => '.*'])]
     public function index(): JsonResponse
     {
         return $this->json([
